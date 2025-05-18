38381cc4857ea3c9cf908d3d0c4ae4a6cc9da04e
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2025-05-18 23:20:53 +0200

    Add packages for wayland clipboard and caps2esc

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ sudo pacman -S --noconfirm base-devel linux-headers xdg-user-dirs
 sudo pacman -S --noconfirm nvidia-dkms nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings
 
 # Other packages
-sudo pacman -S --noconfirm neofetch
+sudo pacman -S --noconfirm neofetch wl-clipboard interception-tools interception-caps2esc
 
 # sudo vim /etc/modprobe.d/nvidia.conf -> options nvidia_drm modeset=1
 # sudo vim /etc/mkinitcpio.conf -> MODULES=(btrfs i915 nvidia nvidia_modeset nvidia_uvm nvidia_drm)
