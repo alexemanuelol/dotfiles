9130af2690d20f6b97d62e8a085400c232e161c5
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2025-05-21 22:51:40 +0200

    [nvim] Make background transparent

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,3 +3,10 @@ vim.g.mapleader = " "
 require("config.lazy")
 
 require("general")
+
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
