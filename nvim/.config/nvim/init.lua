788332074f4a79a80fc029ed23f1d78529b17a95
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2025-05-24 10:39:04 +0200

    [nvim] Update transparent functionality to a more modern standard

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,9 +4,8 @@ require("config.lazy")
 
 require("general")
 
-vim.cmd [[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
-]]
+-- Make background transparent
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })       -- Main text area
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })  -- Floating windows
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })  -- Borders of floats
+vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })        -- Popup menu (autocomplete)
